2683b8c4e2caae99a05ff778bd6e40d918c901c7
Eric Collard <ruffyreborn@gmail.com>
2023-04-28 11:38:56 -0500

    Completed Navbar

---
+++ script.js
@@ -0,0 +1,44 @@
+// #region NAVBAR
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const nav = document.querySelectorAll('.nav');
+
+openBtn.addEventListener('click', () => {
+    nav.forEach(navEl => navEl.classList.add('visible'));
+});
+
+closeBtn.addEventListener('click', () => {
+    nav.forEach(navEl => navEl.classList.remove('visible'));
+});
+// #endregion
+
+// #region NAVBAR ACCORDIAN
+const toggleBtn = document.querySelectorAll('.dropdown-toggle');
+const toggleBtnParents = document.querySelectorAll('.dropdown');
+
+toggleBtn.forEach(btn => {
+    btn.addEventListener('click', ()=>{
+        btn.parentNode.classList.toggle('active')
+    })
+});
+    
+
+
+
+// #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
