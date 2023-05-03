43e619933951e3933accb8a56ceb3a3261316a7e
Eric Collard <ruffyreborn@gmail.com>
2023-05-02 21:06:20 -0500

    Working on header

--- script.js
+++ script.js
@@ -2,18 +2,26 @@
 const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.close-btn');
 const nav = document.querySelectorAll('.nav');
+const slide = document.querySelector('.slide-out');
 
 openBtn.addEventListener('click', () => {
-    nav.forEach(navEl => navEl.classList.add('visible'));
+    if (!openBtn.classList.contains('slide-out')){
+        nav.forEach(navEl => navEl.classList.add('visible'));
+        openBtn.classList.add('slide-out');
+    } else {
+        nav.forEach(navEl => navEl.classList.remove('visible'));
+        openBtn.classList.remove('slide-out');
+    }
 });
 
-closeBtn.addEventListener('click', () => {
-    nav.forEach(navEl => navEl.classList.remove('visible'));
-});
+// closeBtn.addEventListener('click', () => {
+//     nav.forEach(navEl => navEl.classList.remove('visible'));
+//     openBtn.classList.remove('slide-out');
+// });
 // #endregion
 
 // #region NAVBAR ACCORDIAN
-const toggleBtn = document.querySelectorAll('.dropdown-toggle');
+const toggleBtn = document.querySelectorAll('.dropdown-title');
 const toggleBtnParents = document.querySelectorAll('.dropdown');
 
 toggleBtn.forEach(btn => {
@@ -27,7 +35,14 @@ toggleBtn.forEach(btn => {
 
 // #endregion
 
-
+document.body.onscroll = function headerParallax() {  
+    let scrolling = document.scrollingElement.scrollTop;
+    let headerIMG = document.getElementById("header-img");
+    let xPos = "center";
+    let factor = .30;
+    let yPos = (scrolling - 1400) * factor;
+    headerIMG.style.backgroundPosition = xPos + " " + yPos + "px";
+  }
 
 
 
